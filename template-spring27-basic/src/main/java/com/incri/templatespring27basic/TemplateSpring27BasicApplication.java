a7586919b755db47669285bc79271f696ddbcfee
deicide5171 <deicide5564@gmail.com>
2026-02-13 16:00:44 +0900

    asdf

---
+++ template-spring27-basic/src/main/java/com/incri/templatespring27basic/TemplateSpring27BasicApplication.java
@@ -0,0 +1,13 @@
+package com.incri.templatespring27basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateSpring27BasicApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TemplateSpring27BasicApplication.class, args);
+  }
+
+}
---
+++ template-spring27-basic/src/test/java/com/incri/templatespring27basic/TemplateSpring27BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incri.templatespring27basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateSpring27BasicApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
